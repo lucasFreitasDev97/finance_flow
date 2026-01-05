7f5d8be3cd1fdcf87618d93a1c98142285da8725
Lucas Freitas <lucas.rodrigues.developer97@gmail.com>
2026-01-05 20:51:26 -0300

    create routes expenses

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,13 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\ExpenseController;
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/expenses', [ExpenseController::class, 'index'])->name('expenses.index');
+Route::get('/expenses/create', [ExpenseController::class, 'create'])->name('expenses.create');
+Route::post('/expenses', [ExpenseController::class, 'store'])->name('expenses.store');
+
